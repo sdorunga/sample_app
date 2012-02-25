40a7cf3f2208eac95cef2f6c88fce1ab63e75314
Stefan Dorunga <stefandorunga@gmail.com>
2012-02-25 12:26:46 +0000

    fix header missing an erb close tag finish refactoring rspec tests create a spec/support/uttilites.rb file which includes a full_title helper

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,56 +3,34 @@ require 'spec_helper'
 describe "Static pages" do
 
   let(:base_title) {"Ruby on Rails Tutorial Sample App"}
-  subject {page} #tells should that the subject is page so we do not reference it
+  subject { page } #tells should that the subject is page so we do not reference it
 
   describe "Home page" do
     before { visit root_path} #can also be before(:each); applies this action before every other action so we do not
     #put visit _path before each element
 
-    it should have_slector('h1', text: 'Sample App')
-    
-
-    it "should have the right title" do
-  		page.should have_selector('title',
-                    :text => "#{base_title} | Home")
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: full_title('Home')) }
 	end
-  end
 
   describe "Help page" do
     before { visit help_path}
 
-    it "should have the h1 'Help'" do
-      page.should have_slector('h1', text: 'Help')
-    end
-
-    it "should have the right title" do
-  		page.should have_selector('title',
-                    :text => "#{base_title} | Help")
-	end
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path}
 
-    it "should have the h1 'About Us'" do
-      page.should have_slector('h1', text: 'About Us')
-    end
-
-  	it "should have the right title" do
-  		page.should have_selector('title',
-                    :text => "#{base_title} | About Us")
-	end
+    it { should have_selector('h1', text: 'About Us') }
+  	it { should have_selector('title', text: full_title('About Us')) }
   end 
   	
   describe "Contact page" do
     before { visit contact_path} 
       
-    it "should have the h1 'Contact'" do
-      page.should have_slector('h1', text: "Contact")
-  end
-
-    it "should have the right title" do
-      page.should have_selector('title', :text => "#{base_title} | Contact")
-  end
+    it { should have_selector('h1', text: "Contact") }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 end
---
+++ spec/support/utilities.rb
@@ -0,0 +1,9 @@
+#files in the support directory are automatically included by rspec
+def full_title (page_title) 
+	base_title = "Ruby on Rails Tutorial Sample App"
+	if page_title.empty?
+		base_title
+	else
+		"#{base_title} | #{page_title}"
+	end
+end
