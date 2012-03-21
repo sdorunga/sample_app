bdb9380538d0c7509995da95ba34ee8fbef6bef1
Stefan Dorunga <stefandorunga@gmail.com>
2012-03-21 17:28:11 +0000

    change content tests to be more compact, create a _old file to keep old tests and add a logo helper to link to the logo

--- spec/requests/static_pages_spec_old.rb
+++ spec/requests/static_pages_spec_old.rb
@@ -5,46 +5,33 @@ describe "Static pages" do
   let(:base_title) {"Ruby on Rails Tutorial Sample App"}
   subject { page } #tells should that the subject is page so we do not reference it
 
-  shared_examples_for "static pages" do
-    it { should have_selector('h1', text: heading) }
-    it { should have_selector('title', text: full_title(page_title))}
-  end
-
   describe "Home page" do
     before { visit root_path} #can also be before(:each); applies this action before every other action so we do not
     #put visit _path before each element
 
-    let(:heading) {'Sample App'}
-    let(:page_title) {'Home'}
-
-    it_should_behave_like "static pages"
+    it { should have_selector('h1', text: 'Sample App') }
+    it { should have_selector('title', text: full_title('Home')) }
 	end
 
   describe "Help page" do
     before { visit help_path}
 
-    let(:heading) {'Help'}
-    let(:page_title) {'Help'}
-
-    it_should_behave_like "static pages"
+    it { should have_selector('h1', text: 'Help') }
+    it { should have_selector('title', text: full_title('Help')) }
   end
 
   describe "About page" do
     before { visit about_path}
 
-    let(:heading) {'About'}
-    let(:page_title) {'About'}
-
-    it_should_behave_like "static pages"
+    it { should have_selector('h1', text: 'About Us') }
+  	it { should have_selector('title', text: full_title('About Us')) }
   end 
   	
   describe "Contact page" do
     before { visit contact_path} 
       
-    let(:heading) {'Contact'}
-    let(:page_title) {'Contact'}
-
-    it_should_behave_like "static pages"
+    it { should have_selector('h1', text: "Contact") }
+    it { should have_selector('title', text: full_title('Contact')) }
   end
 
   #Test that links are leading to appropriate pages
