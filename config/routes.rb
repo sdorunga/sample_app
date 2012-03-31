2f4b7ce9ea177dfd11466fa6833ca43e42f66ef4
Stefan Dorunga <stefandorunga@gmail.com>
2012-03-31 15:09:34 +0100

    add sessions resource and controller-view-model -- update routes for session managemetn

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 SampleApp::Application.routes.draw do
  
   resources :users #creates a link to users paths and also adds RESTful actions like CRUD
+  resources :sessions, only: [:new, :create, :destroy]
   match '/signup', to: 'users#new'
+
+  match 'signin', to: 'sessions#new'
+  match 'signout', to: 'sessions#destroy', via: :delete
 
   root to: 'static_pages#home' #points localhost/ to static_pages's home controller
 
