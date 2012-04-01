217739aac78aabdcc7049a3ddc548c17a0d1da76
Stefan Dorunga <stefandorunga@gmail.com>
2012-04-01 16:48:53 +0100

    fix flash persistency -- include sessions helper in controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  include SessionsHelper 
+  #by default all helpers are included in views. This include adds
+  #the Sessions helper to all controllers as they all inherit the 
+  #ApplicationController, therefore Sessions becomes usable everywhere.
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,8 +7,18 @@ def create
 	user = User.find_by_email(params[:session][:email])
 	if user && user.authenticate(params[:session][:password])
 	#checks both if the user exists(is non-nill) and uses the authenticate method from has_secure_password which returns true or false
-	
-	render 'new'
+		sign_in user
+		redirect_to user
+	else
+		flash.now[:error] = "Invalid email/password combination!"
+		#use flasn.now instead of just flash because simple flash
+		#only reacts to to a redirect and not just a simple render
+		#which makes the flash appear even on subsequent pages
+		#
+		#flash.now is designed to flash errors on rendered pages,
+		#not on redirects
+		render 'new'
+	end
 end
 
 def destroy
